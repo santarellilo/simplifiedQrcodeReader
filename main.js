154527e9cb6e057ca75726fdee421a07e4609b13
santarellilo <alphax97@me.com>
2019-08-03 16:18:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const constraints = {audio:false, video:{facingMode:"environment"}}
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(stream) {
   video.srcObject = stream;
+  video.play();
 })
 .catch(function(err) {
   return error;
