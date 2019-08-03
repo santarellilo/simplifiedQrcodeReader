e7548f59980244f3589236760b86b1744c27a36c
santarellilo <alphax97@me.com>
2019-08-03 16:21:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 var video = document.getElementById("video");
 var canvasElement = document.getElementById("canvas");
 var canvas = canvasElement.getContext("2d");
+var button = getElementById("scan");
 
 const constraints = {audio:false, video:{facingMode:"environment"}}
 
 //stream function
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(stream) {
-  video.srcObject = stream;
-  video.play();
-})
-.catch(function(err) {
-  return error;
+function stream(){
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(function(stream) {
+  			video.srcObject = stream;
+  			video.play();
+		})
+		.catch(function(err) {
+  			return error;
 });
 
 canvasElement.height = video.videoHeight;
 canvasElement.width = video.videoWidth;
 canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+button.onclick(){
+	stream();
+}
